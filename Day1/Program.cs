a947989a499911e6c13d20ea52b4ac37c25b2e0c
Wiebe van Aken <wiebevaken@gmail.com>
2023-12-10 00:25:06 +0100

    Day1-1

---
+++ Day1/Program.cs
@@ -0,0 +1,19 @@
+﻿var document = new string [] {
+    "1abc2",
+    "pqr3stu8vwx",
+    "a1b2c3d4e5f",
+    "treb7uchet"
+};
+
+var numbers = document
+    .Select(line => {
+        var numbers = line
+            .Select(c => c)
+            .Where(char.IsDigit)
+            .ToList();
+        
+        return Int32.Parse($"{numbers.First()}{numbers.Last()}");
+    })
+    .Sum();
+
+Console.WriteLine(numbers);
